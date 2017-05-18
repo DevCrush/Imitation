bda4faea27e4b023ddea1c1600bf1e915936f2f1
DESKTOP-PEHPDVQ\Crush <yhzqx910622@gmail.com>
2017-05-18 22:57:23 +0800

    BindView annotation search

--- app/src/main/java/com/crush/example/MainActivity.java
+++ app/src/main/java/com/crush/example/MainActivity.java
@@ -1,9 +1,14 @@
 package com.crush.example;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.crush.annotation.BindView;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.tv)
+    TextView mTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- compiler/src/main/java/com/crush/compiler/CusProcessor.java
+++ compiler/src/main/java/com/crush/compiler/CusProcessor.java
@@ -1,6 +1,7 @@
 package com.crush.compiler;
 
 import com.crush.annotation.BindView;
+import com.google.auto.common.SuperficialValidation;
 import com.google.auto.service.AutoService;
 
 import java.lang.annotation.Annotation;
@@ -8,11 +9,22 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.type.TypeVariable;
+import javax.tools.Diagnostic;
+
+import static javax.lang.model.element.ElementKind.CLASS;
+import static javax.lang.model.element.Modifier.PRIVATE;
+import static javax.lang.model.element.Modifier.STATIC;
 
 /**
  * Debug command
@@ -45,15 +57,106 @@ public class CusProcessor extends AbstractProcessor {
         return types;
     }
 
+    Messager messager;
+
     @Override
     public synchronized void init(ProcessingEnvironment processingEnvironment) {
         super.init(processingEnvironment);
+        messager = processingEnvironment.getMessager();
     }
 
     @Override
-    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+    public boolean process(Set<? extends TypeElement> set, RoundEnvironment env) {
+
+        for (Element element : env.getElementsAnnotatedWith(BindView.class)) {
+            if (!SuperficialValidation.validateElement(element)) continue;
+            logv(element.toString());
+            generateBindView(  element.getEnclosingElement());
+        }
+        return true;
+    }
+
+    private void generateBindView(Element element) {
+//        TypeElement enclosingElement = (TypeElement) element.getEnclosingElement();
+        String name = element.getSimpleName().toString();
+        boolean hasError = isInaccessibleViaGeneratedCode(BindView.class, "fields", element)
+                || isBindingInWrongPackage(BindView.class, element);
+
+        // Verify that the target type extends from View.
+        TypeMirror elementType = element.asType();
+        if (elementType.getKind() == TypeKind.TYPEVAR) {
+            TypeVariable typeVariable = (TypeVariable) elementType;
+            elementType = typeVariable.getUpperBound();
+        }
+//        TypeSpec.Builder typeSpec = TypeSpec.classBuilder();//HelloWorld是类名
+    }
+
+    private boolean isInaccessibleViaGeneratedCode(Class<? extends Annotation> annotationClass,
+                                                   String targetThing, Element element) {
+        boolean hasError = false;
+        TypeElement enclosingElement = (TypeElement) element.getEnclosingElement();
+
+        // Verify method modifiers.
+        Set<Modifier> modifiers = element.getModifiers();
+        if (modifiers.contains(PRIVATE) || modifiers.contains(STATIC)) {
+//            error(element, "@%s %s must not be private or static. (%s.%s)",
+//                    annotationClass.getSimpleName(), targetThing, enclosingElement.getQualifiedName(),
+//                    element.getSimpleName());
+            hasError = true;
+        }
+
+        // Verify containing type.
+        if (enclosingElement.getKind() != CLASS) {
+//            error(enclosingElement, "@%s %s may only be contained in classes. (%s.%s)",
+//                    annotationClass.getSimpleName(), targetThing, enclosingElement.getQualifiedName(),
+//                    element.getSimpleName());
+            hasError = true;
+        }
+
+        // Verify containing class visibility is not private.
+        if (enclosingElement.getModifiers().contains(PRIVATE)) {
+//            error(enclosingElement, "@%s %s may not be contained in private classes. (%s.%s)",
+//                    annotationClass.getSimpleName(), targetThing, enclosingElement.getQualifiedName(),
+//                    element.getSimpleName());
+            hasError = true;
+        }
+
+        return hasError;
+    }
+
+    private boolean isBindingInWrongPackage(Class<? extends Annotation> annotationClass,
+                                            Element element) {
+        TypeElement enclosingElement = (TypeElement) element.getEnclosingElement();
+        String qualifiedName = enclosingElement.getQualifiedName().toString();
+
+        if (qualifiedName.startsWith("android.")) {
+//            error(element, "@%s-annotated class incorrectly in Android framework package. (%s)",
+//                    annotationClass.getSimpleName(), qualifiedName);
+            return true;
+        }
+        if (qualifiedName.startsWith("java.")) {
+//            error(element, "@%s-annotated class incorrectly in Java framework package. (%s)",
+//                    annotationClass.getSimpleName(), qualifiedName);
+            return true;
+        }
+
         return false;
     }
 
+    public void logv(String msg) {
+        messager.printMessage(Diagnostic.Kind.NOTE, msg);
+    }
+
+    public void logw(String msg) {
+        messager.printMessage(Diagnostic.Kind.WARNING, msg);
+    }
+
+    public void logi(String msg) {
+        messager.printMessage(Diagnostic.Kind.OTHER, msg);
+    }
+
+    public void loge(String msg) {
+        messager.printMessage(Diagnostic.Kind.ERROR, msg);
+    }
 
 }
