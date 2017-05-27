7e7c70192cf48bce177959e38763c79ed1fb37d4
DevCrush <yhzqx910622@gmail.com>
2017-05-27 17:45:10 +0800

    测试

--- app/src/main/java/com/crush/example/ffmpeg/FFmpegTestActivity.java
+++ app/src/main/java/com/crush/example/ffmpeg/FFmpegTestActivity.java
@@ -10,7 +10,9 @@ import com.crush.annotation.BindView;
 import com.crush.example.R;
 import com.github.hiteshsondhi88.libffmpeg.FFmpeg;
 import com.github.hiteshsondhi88.libffmpeg.FFmpegExecuteResponseHandler;
+import com.github.hiteshsondhi88.libffmpeg.FFmpegLoadBinaryResponseHandler;
 import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
+import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException;
 
 /**
  * Created by Crush on 5/27/17.
@@ -22,16 +24,40 @@ public class FFmpegTestActivity extends AppCompatActivity {
     TextView tv;
 
     String TAG = FFmpegTestActivity.class.getSimpleName();
+    FFmpeg fFmpeg;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ffmpeg_test);
 //        tv.setText(FFmpeg.getConfig());
+
+        fFmpeg = FFmpeg.getInstance(this);
         try {
+            fFmpeg.loadBinary(new FFmpegLoadBinaryResponseHandler() {
+                @Override
+                public void onFailure() {
+                    Log.v("loadBinary", "onFailure");
+                }
+
+                @Override
+                public void onSuccess() {
+                    Log.v("loadBinary", "onSuccess");
+                }
+
+                @Override
+                public void onStart() {
+                    Log.v("loadBinary", "onStart");
+                }
+
+                @Override
+                public void onFinish() {
+                    Log.v("loadBinary", "onFinish");
+                }
+            });
             String cmd = "-version";
             String[] command = cmd.split(" ");
-            FFmpeg.getInstance(this).execute(command, new FFmpegExecuteResponseHandler() {
+            fFmpeg.execute(command, new FFmpegExecuteResponseHandler() {
                 @Override
                 public void onSuccess(String s) {
                     Log.v(TAG, "onSuccess" + s);
@@ -58,6 +84,8 @@ public class FFmpegTestActivity extends AppCompatActivity {
                 }
             });
         } catch (FFmpegCommandAlreadyRunningException e) {
+            e.printStackTrace();
+        } catch (FFmpegNotSupportedException e) {
             e.printStackTrace();
         }
     }
